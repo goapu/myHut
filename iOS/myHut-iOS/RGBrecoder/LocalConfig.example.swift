1890ec028a9d94bf7b8ce49592cf87a72e8ad4a6
Your goapu <gshubro@yahoo.com>
2026-07-09 14:24:20 +0200

    Add local config template

---
+++ iOS/myHut-iOS/RGBrecoder/LocalConfig.example.swift
@@ -0,0 +1,8 @@
+// LocalConfig.example.swift
+// Copy this file, rename the copy to LocalConfig.swift,
+// and replace YOUR_MAC_LOCAL_IP with your Mac's local IP address.
+
+enum LocalConfig {
+    static let serverIP = "YOUR_MAC_LOCAL_IP"
+    static let serverPort = "5001"
+}
